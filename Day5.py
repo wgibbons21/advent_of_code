d50c45092577ac25b3b011d310937d8fe9258406
wgibbons21 <wgibbons@gmail.com>
2015-12-13 16:47:56 -0500

    Testing Git

--- Day5.py
+++ Day5.py
@@ -1,1 +1,35 @@
-test
+import re
+
+fileHandle = open('/Users/waltergibbons/Desktop/Day5.txt')
+contents = fileHandle.readlines();
+
+pattern1= "(\\w)\\1+"
+pattern2= "[aeiou].*[aeiou].*[aeiou].*"
+pattern3= "ab|cd|pq|xy"
+count = 0
+
+print "Has 2 or more of the same character:"
+for i in range(0, contents.__len__()):
+    if re.search(pattern1 , contents[i]):
+        print(contents[i]),
+
+print "\nHas 3 or more vowels:"
+for i in range(0, contents.__len__()):
+    if re.search(pattern2 , contents[i]):
+        print(contents[i]),
+
+print "\nHas naughty pattern:"
+for i in range(0, contents.__len__()):
+    if re.search(pattern3 , contents[i]):
+        print(contents[i]),
+
+
+print "\nAnswers:"
+for i in range(0, contents.__len__()):
+    if re.search(pattern1 , contents[i]):
+        if re.search(pattern2 , contents[i]):
+           if not re.search(pattern3, contents[i]):
+               count +=1
+               print  contents[i],
+
+print count
