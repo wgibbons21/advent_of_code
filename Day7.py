6494a93aa5c46256543c4e2a0c0008bd2db47194
wgibbons21 <wgibbons@gmail.com>
2015-12-15 21:20:24 -0500

    Shared

--- Day7.py
+++ Day7.py
@@ -1,13 +1,31 @@
 fileHandle = open('/Users/waltergibbons/Desktop/Day7.txt')
 
+contents = []
 contents = fileHandle.readlines()
 contents.sort()
-dict
-
-for i in range(contents.__len__()):
+for i in range(0, contents.__len__()):
     contents[i] = contents[i].split()
+dict = {}
 
-    #print contents[i][0][0].isdigit()
+for j in range(0, 100):    #process pre-computed
+    for i in range(0,contents.__len__()):
+        if contents[i][1] == '->' and contents[i][0][0].isdigit():
+            dict[contents[i][2]] =  int(contents[i][0])
 
-    if contents[i].__len__() == 3 and contents[i][0][0].isdigit():
-        print contents[i]
+        if contents[i][1] == '->' and contents[i][0] in dict:
+            dict[contents[i][2]] = int(dict[contents[i][0]])
+        if contents[i][0] == 'NOT' and (contents[i][1] in dict):
+            dict[contents[i][3]] = int(dict[contents[i][1]]) ^ 65535
+        if contents[i][1] == 'LSHIFT' and (contents[i][0] in dict):
+            dict[contents[i][4]] = (65535 & (int(dict[contents[i][0]]) << int(contents[i][2])))
+        if contents[i][1] == 'RSHIFT' and (contents[i][0] in dict):
+            dict[contents[i][4]] = (65535 & (int(dict[contents[i][0]]) >> int(contents[i][2])))
+        if contents[i][1] == 'AND' and contents[i][0] in dict and contents[i][2] in dict:
+            dict[contents[i][4]] = int(dict[contents[i][0]]) & int(dict[contents[i][2]])
+        if contents[i][1] == 'AND' and contents[i][2] in dict and contents[i][0].isdigit():
+            dict[contents[i][4]] = 1 & int(dict[contents[i][2]])
+        if contents[i][1] == 'OR' and contents[i][0] in dict and contents[i][2] in dict:
+            dict[contents[i][4]] = int(dict[contents[i][0]]) | int(dict[contents[i][2]])
+
+print "\nValue of 'a' is", dict['a']
+
